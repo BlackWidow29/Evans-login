48bc3e724c7492b055047048b3512e187440cbc7
Isabelly Dias <isabellydias29@gmail.com>
2023-05-03 16:28:48 -0300

    init project

---
+++ src/main/java/br/com/evans/evanslogin/EvansLoginApplication.java
@@ -0,0 +1,13 @@
+package br.com.evans.evanslogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvansLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvansLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/evans/evanslogin/EvansLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.evans.evanslogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvansLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
